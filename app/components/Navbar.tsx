21d4d49f60667cd89c10e23949e35d239ff002d9
Cider Boi <me@ciderboi.xyz>
2023-07-15 17:21:07 +0530

    Fix Search bar on smaller screens

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,37 +1,67 @@
-'use client'
+"use client";
 
-import { createStyles, Header, Autocomplete, Group, Burger, rem } from '@mantine/core';
-import { useDisclosure } from '@mantine/hooks';
-import { IconBook, IconBook2, IconBooks, IconHomeSearch, IconList, IconSearch, IconSortAscendingLetters } from '@tabler/icons-react';
-import { list } from '../../public/list.json';
+import {
+  createStyles,
+  Header,
+  Autocomplete,
+  Group,
+  Burger,
+  rem,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  IconBook,
+  IconBook2,
+  IconBooks,
+  IconHomeSearch,
+  IconList,
+  IconSearch,
+  IconSortAscendingLetters,
+} from "@tabler/icons-react";
+import { list } from "../../public/list.json";
 
 const useStyles = createStyles((theme) => ({
   header: {
     paddingLeft: theme.spacing.md,
     paddingRight: theme.spacing.md,
-    backgroundColor: '#212f4d',
-    boxShadow: '0 1px 10px rgba(0, 0, 0, 1)',
-    position: 'fixed',
+    backgroundColor: "#212f4d",
+    boxShadow: "0 1px 10px rgba(0, 0, 0, 1)",
+    position: "fixed",
+
+    [theme.fn.smallerThan("xs")]: {
+      paddingLeft: theme.spacing.xs,
+      paddingRight: theme.spacing.xs,
+      height: rem(112),
+    }
   },
 
   inner: {
     height: rem(56),
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 
   links: {
-    [theme.fn.smallerThan('md')]: {
-      display: 'none',
+    [theme.fn.smallerThan("md")]: {
+      display: "none",
     },
   },
 
   search: {
-    [theme.fn.smallerThan('xs')]: {
-      display: 'none',
+    [theme.fn.smallerThan("xs")]: {
+      // move the search bar to the center
+      position: "absolute",
+      left: "50%",
+      transform: "translateX(-50%)",
+      width: "calc(100% - 2rem)",
+      maxWidth: "calc(100% - 2rem)",
+      zIndex: 100,
+      // and below the title
+      top: "calc(100% - 3.5rem)",
+
     },
-    width: '300px',
+    width: "300px",
   },
 }));
 
@@ -41,89 +71,169 @@ interface HeaderSearchProps {
 
 export function HeaderSearch() {
   const { classes } = useStyles();
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
+  let [size, setSize] = useState(0);
 
   let autocomplete: any[];
   // set the data for the autocomplete to search for tags and names
-  autocomplete = list.map(book => book.tags)
-  autocomplete = autocomplete.flat()
-  autocomplete = autocomplete.map(tag => splitCombinedWords(tag))
-  autocomplete = autocomplete.concat(list.map(book => book.name))
-  autocomplete = autocomplete.sort()
-  autocomplete = autocomplete.filter((item, index) => autocomplete.indexOf(item) === index)
+  autocomplete = list.map((book) => book.tags);
+  autocomplete = autocomplete.flat();
+  autocomplete = autocomplete.map((tag) => splitCombinedWords(tag));
+  autocomplete = autocomplete.concat(list.map((book) => book.name));
+  autocomplete = autocomplete.sort();
+  autocomplete = autocomplete.filter(
+    (item, index) => autocomplete.indexOf(item) === index
+  );
 
-
-  const data = list.map(book => book.name)
+  const data = list.map((book) => book.name);
   data.sort();
 
   useEffect(() => {
-    let search
+    let search;
+    setSize(window.innerWidth);
+    console.log(size)
 
     if (value) {
-      search = list.filter(book => book.name.toLowerCase().includes(value.toLowerCase()))
+      search = list.filter((book) =>
+        book.name.toLowerCase().includes(value.toLowerCase())
+      );
       if (search.length === 0) {
-        search = list.filter(book => book.tags.some(tag => tag.toLowerCase().includes(value.toLowerCase())))
+        search = list.filter((book) =>
+          book.tags.some((tag) =>
+            tag.toLowerCase().includes(value.toLowerCase())
+          )
+        );
         if (search.length === 0) {
-          console.log("no books found")
+          console.log("no books found");
         }
       }
 
       // redirect /search with the search results on enter
 
       // listen for enter
-      document.addEventListener('keydown', function (event) {
-        if (event.key === 'Enter') {
-          window.location.href = `/search?query=${value}`
+      document.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+          window.location.href = `/search?query=${value}`;
         }
       });
+
 
       // window.location.href = `/search?query=${value}&results=${JSON.stringify(results)}`
     }
   }, [value]);
 
   return (
-    <Header height={56} className={classes.header} mb={120}>
-      <div className={classes.inner}>
-        <Group style={{
-          fontSize: '1.1rem',
-          fontWeight: '700',
-        }}>
-          <IconBooks size="1.5rem" />
-          Koding Books
-        </Group>
+    <>
+      {size < 768 ? (
+        <Header
+          height={112}
+          className={classes.header}
+          style={{
+            paddingLeft: "1rem",
+            paddingRight: "1rem",
+          }}
+        >
+          <div className={classes.inner}>
+            <Group
+              style={{
+                fontSize: "1.1rem",
+                fontWeight: "700",
+              }}
+            >
+              <IconBooks size="1.5rem" />
+              Koding Books
+            </Group>
 
-        <Group>
-          <Autocomplete
-            className={classes.search}
-            placeholder="Search"
-            icon={<IconSearch size="1rem" stroke={1.5} />}
-            data={autocomplete}
-            limit={10}
-            value={value}
-            onChange={setValue}
-            radius={15}
-            bg={'#212f4d'}
-            rightSection={<div className='kbd kbd-sm'>/</div>}
-          />
-        </Group>
-      </div>
-    </Header>
+            <Group>
+              <Autocomplete
+                className={classes.search}
+                placeholder="Search"
+                icon={<IconSearch size="1rem" stroke={1.5} />}
+                data={autocomplete}
+                limit={10}
+                value={value}
+                onChange={setValue}
+                radius={15}
+                bg={"#212f4d"}
+                rightSection={<div className="kbd kbd-sm">/</div>}
+              />
+            </Group>
+          </div>
+        </Header>
+      ) : (
+        <Header height={56} className={classes.header}>
+          <div className={classes.inner}>
+            <Group
+              style={{
+                fontSize: "1.1rem",
+                fontWeight: "700",
+              }}
+            >
+              <IconBooks size="1.5rem" />
+              Koding Books
+            </Group>
+
+            <Group>
+              <Autocomplete
+                className={classes.search}
+                placeholder="Search"
+                icon={<IconSearch size="1rem" stroke={1.5} />}
+                data={autocomplete}
+                limit={10}
+                value={value}
+                onChange={setValue}
+                radius={15}
+                bg={"#212f4d"}
+                rightSection={<div className="kbd kbd-sm">/</div>}
+              />
+            </Group>
+          </div>
+        </Header>
+      )}
+    </>
+
+    // <Header Header height = { 56} className = { classes.header } mb = { 120} >
+    // <div className={classes.inner}>
+    //   <Group style={{
+    //     fontSize: '1.1rem',
+    //     fontWeight: '700',
+    //   }}>
+    //     <IconBooks size="1.5rem" />
+    //     Koding Books
+    //   </Group>
+
+    //   <Group>
+    //     <Autocomplete
+    //       className={classes.search}
+    //       placeholder="Search"
+    //       icon={<IconSearch size="1rem" stroke={1.5} />}
+    //       data={autocomplete}
+    //       limit={10}
+    //       value={value}
+    //       onChange={setValue}
+    //       radius={15}
+    //       bg={'#212f4d'}
+    //       rightSection={<div className='kbd kbd-sm'>/</div>}
+    //     />
+    //   </Group>
+    // </div>
+    // </Header >
   );
 }
 
-import _ from 'lodash';
-import { MutableRefObject, useEffect, useState } from 'react';
-import { Hash } from 'crypto';
+import _, { size } from "lodash";
+import { MutableRefObject, useEffect, useState } from "react";
+import { Hash } from "crypto";
 
 function splitCombinedWords(str: string) {
-  var words = str.split(/(?=[A-Z])/);  // Split at uppercase letters
+  var words = str.split(/(?=[A-Z])/); // Split at uppercase letters
 
   var result = _.map(words, function (word: string, index: number) {
     if (index > 0 && word === word.toUpperCase()) {
-      return ' ' + word;
+      return " " + word;
     }
     return _.startCase(word.toLowerCase());
   });
 
-  return result.join(' ');
+  return result.join(" ");
 }
